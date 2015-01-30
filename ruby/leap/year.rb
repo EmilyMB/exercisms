19ee3a190363563424bb2e4bb8a842c7ece5e5f6
Emily Berkeley <eberkeley@gmail.com>
2015-01-30 14:55:31 -0700

    leap_v2

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,18 +1,18 @@
 module Year
 
   def self.leap?(year)
-    divisible_by_4(year) &&
-    (not_a_century(year) || four_hundred_century(year))
+    regular_leap_year(year) &&
+    (!regular_century(year) || four_hundred_century(year))
   end
 
   private
 
-  def self.divisible_by_4(year)
+  def self.regular_leap_year(year)
     year % 4 == 0
   end
 
-  def self.not_a_century(year)
-    year % 100 != 0
+  def self.regular_century(year)
+    year % 100 == 0
   end
 
   def self.four_hundred_century(year)
