6570d3f9baa3350d91a6c449a558837dea652881
Emily Berkeley <eberkeley@gmail.com>
2015-02-24 23:01:06 -0700

    iteration_2

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,7 +1,4 @@
 def is_leap_year(year):
-    if(    year % 4 != 0
-       or (year % 100 == 0
-       and year % 400 != 0)):
-        return(False)
-    else:
-        return(True)
+    return (    year % 4 == 0
+            and (year % 100 != 0
+            or year % 400 == 0))
