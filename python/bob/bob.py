a14f3094c929a2ef4b49cd27cc17ffecb7f22e35
Emily Berkeley <eberkeley@gmail.com>
2015-02-24 22:53:06 -0700

    iteration_2

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,17 +1,14 @@
 import re
 
 def hey(what):
-    if(     what.upper() == what
-        and what != ""
-        and re.search('[a-zA-Z]', what)
-       ):
+    if(what.isupper()):
         return('Whoa, chill out!')
 
     elif(   what.endswith("?")
          or what.endswith(" ")):
         return('Sure.')
 
-    elif(what.endswith("\t") or what ==""):
+    elif(what.strip() == ""):
         return('Fine. Be that way!')
 
     else:
