f53160deba65392ee875ee50f343a200759f18c7
Emily Berkeley <eberkeley@gmail.com>
2015-02-27 12:29:18 -0700

    v1

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,41 +1,15 @@
 function words(string) {
-
-  var wordArray = string.split(' ');
-
-  wordArray.forEach(countWords);
-
-}
-  // wordArray.forEach(function (element, index, array) {
-  //   var newArray = {};
-  //
-  //   if (newArray[element] == 'undefined') {
-  //     newArray[element] = 1;
-  //   } else {
-  //     newArray[element] += 1;
-  //   }
-  //   return(newArray);
-  // });
-
-function countWords(element, index, array) {
+  var wordArray = string.split(/[\s]+/);
   var newArray = {};
 
-  if (newArray[element] == 'undefined') {
-    newArray[element] = 1;
-  } else {
-    newArray[element] += 1;
-  }
-  return(newArray);
+  wordArray.forEach(function(word) {
+    if (newArray[word] === undefined) {
+      newArray[word] = 1;
+    } else {
+      newArray[word] += 1;
+    }
+  });
+  return(newArray)
 }
-
-  // [0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, index, array) {
-  //   return currentValue;
-  // });
-
-  // function logArrayElements(element, index, array) {
-  //   console.log('a[' + index + '] = ' + element);
-  // }
-  //
-  // [2, 5, , 9].forEach(logArrayElements);
-
 
 module.exports = words;
