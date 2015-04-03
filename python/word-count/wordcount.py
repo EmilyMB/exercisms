c47d4d04aa142b4579ea1524b8c9d7d7fc612c75
Emily Berkeley <eberkeley@gmail.com>
2015-04-03 10:40:35 -0600

    v3

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,6 +1,4 @@
 def word_count(words):
     words = words.split()
-    word_counts = {}
-    for word in words:
-        word_counts[word] = words.count(word)
-    return(word_counts)
+    word_counts = {word: words.count(word) for word in words}
+    return word_counts
