fa6ad161a577b1440e7cff7981d1539d9733c0c5
Emily Berkeley <eberkeley@gmail.com>
2015-05-26 09:45:59 -0600

    v1 hello world

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,8 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+def hello(name = ''):
+    if name == '':
+        return 'Hello, world!'
+    else:
+        return 'Hello, ' + name + '!'
