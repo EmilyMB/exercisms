2a2eb4eabc992d073e37b6e3a5f78730a8592d7e
Emily Berkeley <eberkeley@gmail.com>
2015-05-27 08:31:52 -0600

    v2 change from string concat to join

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,6 +1,6 @@
 def to_rna(dna_strand):
     dna_to_rna = { 'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    rna_strand = ""
+    rna_strand = []
     for nucleotide in dna_strand:
-        rna_strand += dna_to_rna[nucleotide]
-    return rna_strand
+        rna_strand.append(dna_to_rna[nucleotide])
+    return ('').join(rna_strand)
