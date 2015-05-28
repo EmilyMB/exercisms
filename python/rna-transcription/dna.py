f1fea4750f514c639b4750ea5a78d0268e973bcf
Emily Berkeley <eberkeley@gmail.com>
2015-05-28 09:40:11 -0600

    v3 use translation table

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,6 +1,4 @@
+import string
 def to_rna(dna_strand):
-    dna_to_rna = { 'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    rna_strand = []
-    for nucleotide in dna_strand:
-        rna_strand.append(dna_to_rna[nucleotide])
-    return ('').join(rna_strand)
+    dna_to_rna = string.maketrans('GCTA', 'CGAU')
+    return dna_strand.translate(dna_to_rna)
