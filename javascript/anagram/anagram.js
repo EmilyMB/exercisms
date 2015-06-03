64aeb869c11679fc93c86fb4981ce1e08ba2cd4d
Emily Berkeley <eberkeley@gmail.com>
2015-06-03 12:07:10 -0600

    v1

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,10 +1,22 @@
-var anagram = function(word){
-  this.meaning = word;
+var Anagram = function(input) {
+  this.input = input.toLowerCase();
+  this.matches = function(possible_anagrams) {
+    if (typeof arguments[0] == 'string') {
+      possible_anagrams = Array.prototype.slice.call(arguments);
+    }
+    return possible_anagrams.filter(isAnagram);
+  };
+  return this;
 };
 
-Anagram.prototype.matches = function(input) {
-    return("hi");
-}
+function isAnagram(word) {
+  if(word.toLowerCase() != input) {
+    return sortWord(word) == sortWord(input);
+  } else { return false; }
+};
 
-Anagram = new Anagram();
+function sortWord(word) {
+  return word.toLowerCase().split("").sort().join();
+};
+
 module.exports = Anagram;
