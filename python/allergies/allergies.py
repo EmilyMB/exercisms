50ec334404b62e4a815a0def888c8b980165f10a
Emily Berkeley <eberkeley@gmail.com>
2015-06-03 08:11:56 -0600

    v2 change to enumerate

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,21 +1,21 @@
 class Allergies(object):
     def __init__(self, score):
-        self.score = score
-        self.list = self.create_allergy_list(score)
+        new_score = score % self.max_score()
+        self.list = self.create_allergy_list(new_score)
 
     allergies = ["eggs", "peanuts", "shellfish", "strawberries",
                  "tomatoes", "chocolate", "pollen", "cats"]
+
+    def max_score(self):
+        return 2**len(self.allergies)
 
     def is_allergic_to(self, item):
         return item in self.list
 
     def create_allergy_list(self, score):
-        list = []
-        for i in reversed(xrange(8)):
-            if score == 257:
-                return ["eggs"]
-                break
+        allergy_list = []
+        for i, allergy in reversed(list(enumerate(self.allergies))):
             if score >= 2**i:
-                list.insert(0, self.allergies[i])
+                allergy_list.insert(0, allergy)
                 score -= 2**i
-        return list
+        return allergy_list
