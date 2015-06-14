5dd278e025ee775c498e6341fc7647ed7a1ba8ad
Emily Berkeley <eberkeley@gmail.com>
2015-06-14 08:09:47 -0600

    v1

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,12 @@
+var DNA_TO_RNA = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+
+var toRna = function(strand) {
+  var rna = ''
+  strand.split('').forEach(function(nucleotide) {
+    rna += DNA_TO_RNA[nucleotide]
+  });
+  return rna;
+}
+
+
+module.exports = toRna;
