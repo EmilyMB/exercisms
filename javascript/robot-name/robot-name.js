1eecc925af211b3f464a324111d7e32f84411163
Emily Berkeley <eberkeley@gmail.com>
2015-06-15 12:15:57 -0600

    v1

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,3 +1,15 @@
-var Robot = {};
+var Robot = function() {
+  this.name = randomName();
+};
+
+Robot.prototype.reset = function() {
+  this.name = randomName();
+};
+
+var randomName = function() {
+  var numbers = Math.random().toString(10).substr(2, 3);
+  var letters = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 2);
+  return letters.toUpperCase() + numbers;
+};
 
 module.exports = Robot;
