24c1c385df13fc7e9fa752b14cfe98fc508ec593
Emily Berkeley <eberkeley@gmail.com>
2015-06-19 10:15:47 -0400

    v2 dynamically generate functions

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,46 +3,24 @@ var Age = function(seconds) {
   this.earthAge = seconds / 31557600.00;
 };
 
-Age.prototype.onEarth = function() {
-  return roundNumber(this.earthAge);
+const ageInEarthYears = {
+  'Earth': 1,
+  'Mercury': 0.2408467,
+  'Venus': 0.61519726,
+  'Mars': 1.8808158,
+  'Jupiter': 11.862615,
+  'Saturn': 29.447498,
+  'Uranus': 84.016846,
+  'Neptune': 164.79132
 };
 
-Age.prototype.onMercury = function() {
-  var number = this.earthAge / 0.2408467;
-  return roundNumber(number);
-};
+Object.keys(ageInEarthYears).forEach(function(planet, index) {
+  Age.prototype['on' + planet] = function() {
+    return roundNumber(this.earthAge / ageInEarthYears[planet]);
+  };
+});
 
-Age.prototype.onVenus = function() {
-  var number = this.earthAge / 0.61519726;
-  return roundNumber(number);
-};
-
-Age.prototype.onMars = function() {
-  var number = this.earthAge / 1.8808158;
-  return roundNumber(number);
-};
-
-Age.prototype.onJupiter = function() {
-  var number = this.earthAge / 11.862615;
-  return roundNumber(number);
-};
-
-Age.prototype.onSaturn = function() {
-  var number = this.earthAge / 29.447498;
-  return roundNumber(number);
-};
-
-Age.prototype.onUranus = function() {
-  var number = this.earthAge / 84.016846;
-  return roundNumber(number);
-};
-
-Age.prototype.onNeptune = function() {
-  var number = this.earthAge / 164.79132;
-  return roundNumber(number);
-};
-
-var roundNumber = function(stuff) {
-  return Math.round(stuff * 100) / 100 ;
+var roundNumber = function(number) {
+  return Math.round(number * 100) / 100;
 };
 module.exports = Age;
