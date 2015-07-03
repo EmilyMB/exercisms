039d7416930a896e6426b22511af19b4185a64f9
Emily Berkeley <eberkeley@gmail.com>
2015-07-03 10:05:26 -0400

    v2 pull out clock object

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,34 +1,36 @@
+var Clock = function(minutes) {
+  this.totalMinutes = minutes;
+};
+
 function at(hour, minutes) {
   minutes = typeof minutes !== 'undefined' ? minutes : 0;
-  var clock = Object.create(null);
   var totalMinutes = 60 * hour + minutes;
 
-  function mod(n, m) {
-    return ((n % m) + m) % m;
-  }
-
-  clock.toString = function() {
-    var newHour = Math.floor(totalMinutes / 60) % 24;
-    var newMinutes = totalMinutes % 60;
-    return ('00' + newHour).substr(-2,2) + ':' + ('00' + newMinutes).substr(-2,2);
-  },
-
-  clock.plus = function(addedMinutes) {
-    totalMinutes = totalMinutes + addedMinutes;
-    return clock;
-  },
-
-  clock.minus = function(minusMinutes) {
-    var minutesInDay = 24 * 60;
-    totalMinutes = mod((totalMinutes - minusMinutes), minutesInDay);
-    return clock;
-  },
-
-  clock.equals = function(clock2) {
-    return clock.toString() === clock2.toString();
-  }
-
-  return Object.create(clock);
+  return new Clock(totalMinutes);
 }
 
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+Clock.prototype.toString = function() {
+  var newHour = Math.floor(this.totalMinutes / 60) % 24;
+  var newMinutes = this.totalMinutes % 60;
+  return ('00' + newHour).substr(-2,2) + ':' + ('00' + newMinutes).substr(-2,2);
+}
+
+Clock.prototype.plus = function(addedMinutes) {
+  this.totalMinutes += addedMinutes;
+  return this;
+}
+
+Clock.prototype.minus = function(minusMinutes) {
+  var minutesInDay = 24 * 60;
+  this.totalMinutes = mod((this.totalMinutes - minusMinutes), minutesInDay);
+  return this;
+}
+
+Clock.prototype.equals = function(clock2) {
+  return this.toString() === clock2.toString();
+}
 module.exports.at = at;
